be287baa42885c80ce66e05c58c59b8f042e2e9a
Uzair Rehman <rehmanuzair43@gmail.com>
2024-04-05 01:22:45 +0500

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,27 +10,22 @@ const Home = () => {
         <Layout>
          <Experience/>
         </Layout>
-      </>
-    );
-  };
-  export default Home;import Layout from "../components/Layout";
-import Head from "next/head";
-import Icon from "../components/Model";
-const Home = () => {
-    
-    return (
-      
-        
     <div className="wrap">
       <Icon className="scrolling-icon" />
       
-      <div className="page1"></div>
+      {/* <div className="page1"></div> */}
    
       </div>
+      </>
+    );
+  };
+
+      
+        
     
 
       
 
-    );
-  };
+    
+  
   export default Home;
